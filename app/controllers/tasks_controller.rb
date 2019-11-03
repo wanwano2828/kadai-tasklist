f320f8acd2ea02991bedeab1c51deedb20732fc8
wanwano2828 <wanwano.2828@keio.jp>
2019-11-03 13:10:59 +0000

    add_correct_user_edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :update, :show]
+  before_action :correct_user, only: [:destroy, :update, :show, :edit]
   
   def index
     if logged_in?
